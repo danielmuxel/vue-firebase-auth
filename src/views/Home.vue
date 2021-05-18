70d81e1302fbdf1b2b793fd5e57f5a61320f0f91
Daniel Muxel <daniel.muxel@gmail.com>
2021-05-18 11:23:37 +0200

    basic home site with info

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1>Home</h1>
+  <h1 class="title">Home</h1>
+  <p>This is an example for authentication with <a href="https://vuejs.org/">Vue.js</a>, <a href="https://vuex.vuejs.org/">Vuex</a> and <a href="https://firebase.google.com/">Firebase</a>.</p>
+  <p>As frontend Framework <a href="https://bulma.io/">bulma</a> is installed.</p>
+  <p>Source code is available on <a href="https://github.com/danielmuxel/vue-firebase-auth">GitHub</a>.</p>
 </template>
 
 <script>
