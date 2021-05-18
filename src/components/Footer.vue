a4e659b6d5f5e0258f487602401421297a685db2
Daniel Muxel <daniel.muxel@gmail.com>
2021-05-18 11:22:23 +0200

    Added a footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      <strong>Vue-Firebase-Auth</strong> by <a href="https://github.com/danielmuxel">Daniel Muxel</a>.
+    </p>
+  </div>
+</footer>
+</template>
