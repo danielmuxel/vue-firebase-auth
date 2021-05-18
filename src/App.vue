a5d3a7e8215f0b5d56e6621932daeb8929588a3a
Daniel Muxel <daniel.muxel@gmail.com>
2021-05-18 11:22:38 +0200

    Updates layout and put footer at the bottom

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,36 @@
 <template>
-  <Navbar />
-  <router-view/>
+  <main>
+    <Navbar />
+    <section class="section">
+      <div class="container">
+        <router-view />
+      </div>
+    </section>
+    <Footer />
+  </main>
 </template>
 
 <script>
-import Navbar from './components/Navbar'
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 export default {
   components: {
-    Navbar
-  }
-}
+    Navbar,
+    Footer,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+/* Make footer stick to the bttom */
+main {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+main > footer {
+  margin-top: auto;
 }
 </style>
